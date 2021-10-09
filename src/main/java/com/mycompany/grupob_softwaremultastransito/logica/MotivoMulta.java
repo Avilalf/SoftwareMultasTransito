2c4c381f282c8ce3659022dd1c0825a0cdbe0fae
Avilalf <avilalfonsin@gmail.com>
2021-10-08 23:45:29 -0500

    Se inicia proyecto, se crean las clases Multa y MotivoMulta sin relaciones

---
+++ src/main/java/com/mycompany/grupob_softwaremultastransito/logica/MotivoMulta.java
@@ -0,0 +1,25 @@
+/* 
+ * — Autor: Cristian David Avila Narvaez
+ * — Código de estudiante: 1957500-2711
+ * — Programación Interactiva.
+ * — Grupo: D.
+ */
+package com.mycompany.grupob_softwaremultastransito.logica;
+
+/**
+ *
+ * @author avila
+ */
+public class MotivoMulta {
+
+    private short codigo;
+    private String descripcion;
+    private int valor;
+
+    public MotivoMulta(short codigo, String descripcion, int valor) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.valor = valor;
+    }
+
+}
---
+++ src/main/java/com/mycompany/grupob_softwaremultastransito/logica/Multa.java
@@ -0,0 +1,21 @@
+/* 
+ * — Autor: Cristian David Avila Narvaez
+ * — Código de estudiante: 1957500-2711
+ * — Programación Interactiva.
+ * — Grupo: B.
+ */
+package com.mycompany.grupob_softwaremultastransito.logica;
+
+/**
+ *
+ * @author avila
+ */
+public class Multa {
+
+    private int valor;
+
+    public Multa(int valor) {
+        this.valor = valor;
+    }
+
+}
